dc2793cec6fbfc5a8cd19011240361ec443d7942
Omar <omar.sk80@gmail.com>
2024-04-27 22:27:01 -0400

    onclick

---
+++ js/onClick.js
@@ -0,0 +1,7 @@
+// Selecciona el botón por su id
+var boton = document.getElementById("btnUnete");
+
+// Agrega la función que se ejecutará cuando se haga clic en el botón
+boton.onclick = function() {
+  alert("¡CUPOS ABIERTOS EN MAYO!");
+};
